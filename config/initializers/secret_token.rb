0faca0c689df1caced09f310d785a87ba54305c0
Eduardo Cobuci <dudu@MacDudu.local>
2010-10-19 01:24:47 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorksForMe::Application.config.secret_token = '247f85e85837bfa2695e2129679496407913ad553a79d9d9d5a5505afa9fb890740d35c099666ccd44b96e8ddc50ca963b9db98fad391f57393fa2791341f00d'
