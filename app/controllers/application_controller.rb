89086fa09fbca97c7abb4c2f6c4a6833ec4a3099
Eduardo Cobuci <dudu@MacDudu.local>
2010-11-04 04:42:34 -0200

    partials moved to shared folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :load_side_menu_data, :load_main_menu_data
+  before_filter :load_latest_posts, :load_side_menu_data, :load_main_menu_data
   
   private
+  
+  def load_latest_posts
+    @posts = Post.order("id desc").take(5)
+  end
   
   def load_side_menu_data
     @categories = Category.order("name")
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   respond_to :html
   
   def index
-    @posts = Post.all
     respond_with @posts    
   end
 
