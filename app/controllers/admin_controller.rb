a85655d12e97275ad91411b447069a157ea57a5f
Eduardo Cobuci <educobuci@gmail.com>
2011-02-27 13:41:33 -0300

    branch test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,9 +6,9 @@ class AdminController < ActionController::Base
   before_filter :authenticate
     
   private
-     def authenticate
-        authenticate_or_request_with_http_basic do |id, password| 
-            id == USER_ID && password == PASSWORD
-        end
-     end
+  def authenticate
+    authenticate_or_request_with_http_basic do |id, password| 
+      id == USER_ID && password == PASSWORD
+    end
+  end
 end
