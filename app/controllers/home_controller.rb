45564d889fa6f59a173c8a1fbb5d4d0336ea9282
Eduardo Cobuci <educobuci@gmail.com>
2011-02-27 23:34:27 -0300

    microformats, meta desc, proper heading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
+# coding: utf-8
 class HomeController < ApplicationController
   respond_to :html, :xml
-
-  def index
-    @posts = Post.order("created_at DESC").take(10);
-    respond_with @posts
-  end
   
+  def index
+    @home = true
+    @description = "Dicas, tutoriais e notícias sobre desenvolvimento e tecnologia da informação"
+    @posts = Post.order("created_at DESC").take(10)
+    respond_with @posts
+  end  
 end
